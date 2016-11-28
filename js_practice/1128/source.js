ce64142120b2b62d15f3c6e2c42360b2bd5b4707
machi-box <okame0904@gmail.com>
2016-11-28 23:06:16 +0900

    no message

---
+++ js_practice/1128/source.js
@@ -0,0 +1,15 @@
+
+myTotal = 0;
+myInput = "";
+myCalc = "+";
+myFlg = 1;
+
+function input(myData){
+    console.log("hogehoge");
+    myTotal += myData;
+    console.log(myTotal);
+    
+};
+
+
+document.keisan.txtb.value="ウワアン";
